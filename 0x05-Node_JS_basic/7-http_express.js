2e1987d5f6cb055895c2229301f6cddf41ba4971
Abel <abelbekele.addise@gmail.com>
2023-03-14 06:06:37 +0300

    Update code

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,31 +1,22 @@
 const express = require('express');
 const countStudents = require('./3-read_file_async');
 
-const app = express();
 const port = 1245;
 
-app.get('/', (req, res) => res.send('Hello Holberton School!'));
-app.get('/students', ((req, res) => {
+const app = express().get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  res.write('This is the list of our students\n');
   countStudents(process.argv[2])
-    .then((v) => {
-      res.writeHead(200);
-      res.write('This is the list of our students\n');
-      res.write(`Number of students: ${v.counter}\n`);
-      let len = 0;
-      for (const key in v.inFields) {
-        if (Object.prototype.hasOwnProperty.call(v.inFields, key)) {
-          res.write(`Number of students in ${key}: ${v.inFields[key].counter}. List: ${v.inFields[key].students}`);
-          if (len !== Object.keys(v.inFields).length - 1) res.write('\n');
-          len += 1;
-        }
-      }
-      res.end();
+    .then((data) => {
+      res.end(data);
     })
-    .catch(() => {
-      res.write('This is the list of our students\n');
-      res.status(404).end('Cannot load the database');
+    .catch((error) => {
+      res.end(error.message);
     });
-}));
+});
 
 app.listen(port);
 
