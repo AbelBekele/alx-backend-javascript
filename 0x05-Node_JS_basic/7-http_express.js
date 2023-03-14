fc99fd47fffea0840af620d5b6f333a542434822
Abel <abelbekele.addise@gmail.com>
2023-03-14 05:55:13 +0300

    Update code

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -2,19 +2,31 @@ const express = require('express');
 const countStudents = require('./3-read_file_async');
 
 const app = express();
+const port = 1245;
 
 app.get('/', (req, res) => res.send('Hello Holberton School!'));
+app.get('/students', ((req, res) => {
+  countStudents(process.argv[2])
+    .then((v) => {
+      res.writeHead(200);
+      res.write('This is the list of our students\n');
+      res.write(`Number of students: ${v.counter}\n`);
+      let len = 0;
+      for (const key in v.inFields) {
+        if (Object.prototype.hasOwnProperty.call(v.inFields, key)) {
+          res.write(`Number of students in ${key}: ${v.inFields[key].counter}. List: ${v.inFields[key].students}`);
+          if (len !== Object.keys(v.inFields).length - 1) res.write('\n');
+          len += 1;
+        }
+      }
+      res.end();
+    })
+    .catch(() => {
+      res.write('This is the list of our students\n');
+      res.status(404).end('Cannot load the database');
+    });
+}));
 
-app.get('/students', async (req, res) => {
-  const title = 'This is the list of our students\n';
-  try {
-    const data = await countStudents(process.argv[2]);
-    res.send(`${title}${data.join('\n')}`);
-  } catch (error) {
-    res.send(`${title}${error.message}`);
-  }
-});
-
-app.listen(1245, () => console.log('Server is running...'));
+app.listen(port);
 
 module.exports = app;
